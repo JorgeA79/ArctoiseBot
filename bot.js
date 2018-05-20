1700ccb0f383169dbca07c8bc6027e9a1b5ff4d6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-05-20 16:50:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'help')) {
 	
-		 message.channel.send(message.author.toString() + `, need some help? :thinking: \n **-a!channel**\n **-a!lastvideo**\n **-a!randomvideo**\n **-a!invite**\n **-a!server**\n **-a!ping**`);
+		 message.channel.send(`:wrench: Help Menu\n **-a!channel**\n **-a!lastvideo**\n **-a!randomvideo**\n **-a!invite**\n **-a!server**\n **-a!ping**`);
 	}
 });
 client.on('message', message => {
